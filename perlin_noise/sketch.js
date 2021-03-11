63686e83891e1203ebaaeb37ebd4c6aa68d5166b
Rafael Vital <rvitall.alves@gmail.com>
2021-03-11 01:25:59 -0300

    a moving shape in 2d space

--- perlin_noise/sketch.js
+++ perlin_noise/sketch.js
@@ -1,7 +1,18 @@
+var noiseOffset1 = 0;
+var noiseOffset2 = 10000;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(51);
+
+  let x = noise(noiseOffset1) * width;
+  let y = noise (noiseOffset2) * height;
+  noiseOffset1 += 0.01;
+  noiseOffset2 += 0.01;
+
+  ellipse(x, y, 20,20)
+
 }
