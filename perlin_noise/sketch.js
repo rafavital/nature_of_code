b8a4125e8e082e5c64ff5edaaf0afbc24564973c
Rafael Vital <rvitall.alves@gmail.com>
2021-03-11 02:09:15 -0300

    2d noise

--- perlin_noise/sketch.js
+++ perlin_noise/sketch.js
@@ -1,27 +1,29 @@
-var i = 0.01
+var inc = 0.01
 var start = 0
 function setup() {
   createCanvas(400, 400);
   pixelDensity(1)
 }
 
-function draw() {
-  background(51);
+function draw() {  
+
   
+  let yoff = 0
   loadPixels()
-  xoff = start;
-
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
+  for (let y = 0; y < height; y++) {
+    let xoff = 0
+    for (let x = 0; x < width; x++) {
       let id = (x + y * width) * 4
-      pixels[id]      = x //red 
-      pixels[id + 1]  = random(0,255) //green
-      pixels[id + 2]  = random(0,255) //blue
-      pixels[id + 3]  = 255 //alpha
+      let r = noise (xoff, yoff) * 255
+      pixels[id]      = r
+      pixels[id + 1]  = r
+      pixels[id + 2]  = r
+      pixels[id + 3]  = 255 
+      
+      xoff += inc
     }
+    yoff += inc
   }
-
-  start += i
-
+  
   updatePixels()
 }
