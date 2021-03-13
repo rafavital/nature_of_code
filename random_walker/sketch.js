820505d8ded4b18205180f6a0d046ea8dc972c81
Rafael Vital <rvitall.alves@gmail.com>
2021-03-13 03:00:07 -0300

    simple random walker

--- random_walker/sketch.js
+++ random_walker/sketch.js
@@ -1,15 +1,34 @@
 
-var pos = createVector(0,0)
+var x,y
 function setup() {
   createCanvas(400, 400);
-  pixelDensity(1)
+  x = width / 2;
+  y = height / 2;
+  background(50)
 }
 
 function draw() {  
-  background(50)
-  loadPixels()
-  pos.x = width / 2
-  pos.y = height / 2
-  pixels[pos.x * pos.y * 4] = 255
-  
+  stroke (255)
+  strokeWeight(2)
+  point(x, y)
+
+  let r = floor (random(4))
+
+  switch (r) {
+    case 0:
+      x ++;
+      break;
+
+    case 1:
+      x--;
+      break;
+
+    case 2:
+      y++      
+      break;
+    
+    case 3:
+      y--;
+      break;
+  }
 }
