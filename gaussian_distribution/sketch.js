a26b703fba59a304f90a7e6dae15c1fab689f77a
Rafael Vital <rvitall.alves@gmail.com>
2021-06-11 04:21:51 -0300

    paint splatter sim

--- gaussian_distribution/sketch.js
+++ gaussian_distribution/sketch.js
@@ -1,15 +1,23 @@
+var rMean, gMean, bMean
 var sd = 60
-var mean = 320
 
 function setup() {
-  createCanvas(640, 360);
-  background(50);
+  createCanvas(640, 360)
+  background(50)
+  rMean = random(0, 255)
+  gMean = random(0, 255)
+  bMean = random(0, 255)
 }
 
 function draw() {
-  var num = randomGaussian(mean, sd)
+  var x = randomGaussian(320, sd)
+  var y = randomGaussian(180, sd)
+  var r = randomGaussian(rMean, sd)
+  var g = randomGaussian(gMean, sd)
+  var b = randomGaussian(bMean, sd)
+  var a = randomGaussian(255, 30)
 
-  noStroke();
-  fill(255, 10)
-  ellipse(num, 180, 16, 16);
+  noStroke()
+  fill(r, g, b, a)
+  ellipse(x, y, 20, 20)
 }
