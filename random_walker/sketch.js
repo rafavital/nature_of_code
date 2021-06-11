1398e6c074865b3e37fe5aa9d7ce168910089742
Rafael Vital <rvitall.alves@gmail.com>
2021-06-11 03:52:58 -0300

    experiences with the random walker

--- random_walker/sketch.js
+++ random_walker/sketch.js
@@ -1,33 +1,63 @@
+let walker1
+let x,y
+let r,g,b
 
-var x,y
 function setup() {
-  createCanvas(400, 400);
-  x = width / 2;
-  y = height / 2;
-  background(50)
+  createCanvas(400,400);
+  pixelDensity(1)
+  
+  x = random(400)
+  y = random(400)
+  r = random(255)
+  g = random(255)
+  b = random(255)
+  
+  background(51)  
 }
 
 function draw() {  
-  stroke (255)
-  strokeWeight(2)
-  point(x, y)
+  loadPixels()
+  
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      if (x == this.x && y == this.y)
+      {
+      let pixel_id = (x + y * width) * 4
 
-  let r = floor (random(4))
+      pixels[pixel_id]     = r
+      pixels[pixel_id + 1] = g
+      pixels[pixel_id + 2] = b
+      pixels[pixel_id + 3] = 255
+        }
+    }
+    
+  }
 
-  switch (r) {
-    case 0:
+  let pixel_id = (x + y * width) * 4
+
+  pixels[pixel_id]     = r
+  pixels[pixel_id + 1] = g
+  pixels[pixel_id + 2] = b
+  pixels[pixel_id + 3] = 255
+
+  updatePixels()
+
+  let rand = floor (random(4))
+
+  switch (rand) {
+      case 0:
       x ++;
       break;
 
-    case 1:
+      case 1:
       x--;
       break;
 
-    case 2:
+      case 2:
       y++      
       break;
-    
-    case 3:
+      
+      case 3:
       y--;
       break;
   }
